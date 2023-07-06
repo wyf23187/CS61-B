1eb15c78df2dfb2fb460197d81e2ff8250b65e91
wyf23187 <wyf23187@gmail.com>
2023-07-06 13:20:51 +0800

    lab4B

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,7 +5,7 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 128){
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
